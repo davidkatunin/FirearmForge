7cafda5c762a5c1ad387524f21991b481ae1e7fe
davidkatunin <davidkatunin@gmail.com>
2023-12-24 22:26:27 -0600

    First Progress made on Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,21 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import './Navbar.css';
 
 function Navbar() {
   return (
-    <nav className="navbar">
+    <nav className="navbar_top">
         <div className="navbar-container">
-            <Link className="navbar-logo">
-                LOGO HERE
-            </Link>
+            <a href="#" className="navbar-logo"><s className="logo-red">Firearm</s><s className="logo-orange">Forge</s></a>
           <ul className="nav_user-info">
-            <li className="nav_account">
-              
+            <li className="nav_account li_top">
+              <button className="nav_account-btn">Account</button>
             </li>
-            <li className="nav_state">
-
+            <li className="nav_state li_top">
+              State
             </li>
-            <li className="nav_mode">
-
+            <li className="nav_mode li_top">
+              Switch
             </li>
           </ul>
         </div>
